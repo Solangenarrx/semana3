c2610f8039109013dfdc6edebebcedc664eecb0d
Solange <solangenarrx@gmail.com>
2025-04-09 10:25:53 -0500

    cambios desde vs

--- EJERCICIO1/Program.cs
+++ EJERCICIO1/Program.cs
@@ -13,6 +13,7 @@ namespace EJERCICIO1
             Console.Write("Bienvenidos al curso de C#");
             Console.Write("Bienvenidos al curso de C#");
             Console.Write("Bienvenidos al curso de C#");
+            Console.Write("Bienvenidos al curso de C#");
             Console.ReadKey();
         }
     }
