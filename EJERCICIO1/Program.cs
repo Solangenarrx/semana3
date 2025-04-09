45797f3df36cf9f8ecdeaf38f73eaca2b138970f
Solange <solangenarrx@gmail.com>
2025-04-09 10:21:40 -0500

    tercer commit

--- EJERCICIO1/Program.cs
+++ EJERCICIO1/Program.cs
@@ -12,6 +12,7 @@ namespace EJERCICIO1
         {
             Console.Write("Bienvenidos al curso de C#");
             Console.Write("Bienvenidos al curso de C#");
+            Console.Write("Bienvenidos al curso de C#");
             Console.ReadKey();
         }
     }
